ee1920a5bda35c1462effababe2fb42266533f24
Felipe Marques Megale <fmegale@ibm.com>
2019-09-06 10:31:29 -0300

    1 - watson integration

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,9 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const analyzer = require('./services/toneAnalyzer');
 
+let messagesToAnalyze = [];
 
 app.get('/', function(req, res){
   res.send('<h1>Felipe Megale, Guilherme Galvão, João Castro</h1>');
@@ -12,7 +14,7 @@ io.on('connection', function(socket){
     socket.on('chat message', function (msg) {
         console.log('message: ' + JSON.stringify(msg, null, 4));
         io.emit('chat message', msg);
-    })
+    });
   });
   
 
---
+++ src/server/services/toneAnalyzer.js
@@ -0,0 +1,17 @@
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: "2019-09-06",
+  iam_apikey: "f-v72uGQzuKODHnCZkwr5eEONlw--KspgqHE_GaAiZjc",
+  url: "https://gateway.watsonplatform.net/tone-analyzer/api"
+});
+
+module.exports = {
+  analyze: textToAnalyze => {
+    let requestData = {
+      tone_input: {"text": textToAnalyze},
+      content_type: "application/json"
+    }
+
+    return toneAnalyzer.tone(requestData);
+  }
+}
