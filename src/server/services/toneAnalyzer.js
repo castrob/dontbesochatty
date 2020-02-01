46c02be30cb9e9b5bf42dd29b2c838d0950ef5db
Joao Castro <jpcbpereira@sga.pucminas.br>
2020-02-01 02:37:39 -0300

    Update toneAnalyzer.js

--- src/server/services/toneAnalyzer.js
+++ src/server/services/toneAnalyzer.js
@@ -1,7 +1,7 @@
 const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
 const toneAnalyzer = new ToneAnalyzerV3({
   version: "2019-09-06",
-  iam_apikey: "f-v72uGQzuKODHnCZkwr5eEONlw--KspgqHE_GaAiZjc",
+  iam_apikey: "TOKEN KEY",
   url: "https://gateway.watsonplatform.net/tone-analyzer/api"
 });
 
